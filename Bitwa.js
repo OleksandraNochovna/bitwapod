56d76d857875662e64ddfee4e653641617efc994
OleksandraNochovna <113239824+OleksandraNochovna@users.noreply.github.com>
2023-10-11 22:22:06 +0200

    Add files via upload

--- Bitwa.js
+++ Bitwa.js
@@ -4,4 +4,11 @@ let themeButton = document.querySelector('.theme-button');
 themeButton.onclick = function() {
   page.classList.toggle('light-theme');
   page.classList.toggle('dark-theme');
+
+
+  if (page.classList.contains('dark-theme')) {
+    themeButton.textContent = "☾";
+  } else {
+    themeButton.textContent = "🌣";
+  }
 };
